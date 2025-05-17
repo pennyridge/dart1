77b3bd93fcaf81f5ff067ca988688130dfb6554e
Penny Ridge <pennyridge@sent.com>
2025-05-17 15:58:33 -0500

    Create 1040.dart

---
+++ 1040.dart
@@ -0,0 +1,36 @@
+void main() {
+  
+  // VAR DATATYPE RETAINS DATATYPE OF VALUE ASSIGNED TO IT
+  
+  // create variable of the datatype 'var'
+  // assign a string to the variable
+  var myvar1 = "text";
+  
+  print(myvar1);
+  
+  // reassign the 'myvar1' variable with a different string
+  // value, this time with a string of numbers
+  
+  myvar1 = "123";
+  
+  print(myvar1);
+  
+  // create a new variable of dataype 'var'
+  // assign an integer value to the variable
+  
+  var myvar2 = 123;
+  
+  print(myvar2);
+  
+  // reassign the variable 'myvar2' with a new integer
+  
+  myvar2 = 789;
+  
+  print(myvar2);
+  
+  // uncomment the code below and see what happens
+  
+  // myvar2 = myvar1;
+  // print(myvar2);
+
+}
