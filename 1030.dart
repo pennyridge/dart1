9e9fbe41934f8b93e847ce930cc3e794ffba1b2e
Penny Ridge <pennyridge@sent.com>
2025-05-17 15:01:29 -0500

    Create 1030.dart

---
+++ 1030.dart
@@ -0,0 +1,34 @@
+void main() {
+  
+// DATATYPES in DART
+
+// bool :: for storing only the value true or false
+// var :: for storing any value
+// num :: for storing decimal or integer values
+// int :: for storing whole number, not decimal, values
+// double :: for storing decimal values only
+// String :: for storing text which can be letters and numbers
+  
+  
+String item1 = "Simple";
+String item2 = "strings can be many letters and numbers";
+var item3 = "var can hold any type of data";
+var item4 = 1.23;
+bool item5 = true;
+bool item6 = false;
+int item7 = 786;
+double item8 = 3.14;
+num item9 = 8.43;
+
+print("$item1 $item2 and $item3.");
+print("For example, a decimal value of $item4.");
+print("Although it is $item5 a datatype of double is better");
+print("for storing numbers like $item8.");
+print("When you don't know if you need a decimal or integer datatype");
+print("you can just use the 'num' datatype, for example, $item9.");
+print("But if you know you need a non-decimal number, use the 'int' datatype,");
+print("like in the case of the number $item7.");
+print("Do you like how this was formatted? True or $item6?");
+
+  
+}
