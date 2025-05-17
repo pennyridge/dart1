335ca5f26f694e7876491d25360763229ac18089
pennyridge <pennyridge@sent.com>
2025-05-17 13:07:51 -0500

    Create 1020.dart

---
+++ 1020.dart
@@ -0,0 +1,40 @@
+void main () {
+  
+  double firstnum;
+  double secondnum;
+  
+  // declare our variables but do not give them values
+  // note the use of the word "double" which means "decimal"
+  // because these variables will be used to hold numbers that
+  // will be used in calculations that might result in decimal
+  // values
+  
+  double added;
+  double subtracted;
+  double multiplied;
+  double divided;
+  
+  // declare the variables but do not give them values
+  // we use the "double" datatype because thes variables
+  // will be used to store the result of calculations that
+  // could result in a decimal
+  
+  firstnum = 19;
+  secondnum = 27;
+  
+  // assign the variables actual values to be used later
+  
+  added = firstnum + secondnum;
+  subtracted = secondnum - firstnum;
+  multiplied = firstnum * secondnum;
+  divided = secondnum / firstnum;
+  
+  // assign the variables with the result of "expressions"
+  // which are another name for calculations or series of
+  // actions which produce a result that can be assigned
+  // to a variable
+  
+  print("$added $subtracted $multiplied $divided");
+  
+  
+}
